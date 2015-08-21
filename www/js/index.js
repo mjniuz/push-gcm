a83c5b8431aef9463283bc8b6df73ba635db4da0
Alan Elnino <mail@mjniuz.com>
2015-08-21 21:32:17 +0700

    upload source

--- www/js/index.js
+++ www/js/index.js
@@ -87,6 +87,7 @@ var app = {
 				break;
 
 			case 'message':
+				navigator.notification.alert(e.message);
 				// this is the actual push notification. its format depends on the data model from the push server
 				alert('message = '+e.message+' msgcnt = '+e.msgcnt);
 				break;
